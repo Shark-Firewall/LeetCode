299be3ccadd754731fc80a1687d48bc70c02a73d
Rajnish <Kumarrajnish363.rk@gmail.com>
2024-02-24 03:07:44 +0530

    Time: 2 ms (67.46%) | Memory: 2.4 MB (45.45%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.go
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.go
@@ -1,14 +1,3 @@
 func arrayStringsAreEqual(word1 []string, word2 []string) bool {
-    string1 := "";
-    string2 := "";
-    for _, str := range word1{
-        string1 += str;
-    }
-    for _, str := range word2{
-        string2 += str;
-    }
-    if string1 == string2{
-        return true;
-    }
-    return false;
+    return strings.Join(word1, "") == strings.Join(word2, "");
 }
